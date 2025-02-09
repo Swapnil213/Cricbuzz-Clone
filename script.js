99c1492e0951d12eb7ef9dad52b1151a13d1c12f
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-02-09 21:20:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var btmbutton = document.querySelectorAll(".footer .elem");
+var matchInfo = document.querySelectorAll(".match");
+var back = document.querySelector("#back-btn");
 
 btmbutton.forEach(function (elem) {
     elem.addEventListener("click", function () {
         var page = elem.getAttribute("data-page");
         window.location.href = `${page}.html`;
     })
+})
+
+matchInfo.forEach(function (match) {
+    match.addEventListener("click", function () {
+        var page = match.getAttribute("data-match-name");
+        console.log(page);
+        window.location.href = `match.html`;
+    })
+})
+
+back.addEventListener("click", function () {
+    window.location.href = `index.html`;
 })
