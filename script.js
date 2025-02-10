c501d72acf1b12261e3dce9f1a4b5a5efcf849df
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-02-10 23:34:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var btmbutton = document.querySelectorAll(".footer .elem");
 var matchInfo = document.querySelectorAll(".match");
 var back = document.querySelector("#back-btn");
+var navlinks = document.querySelectorAll(".navbar2 .nav-linksi");
 
 btmbutton.forEach(function (elem) {
     elem.addEventListener("click", function () {
@@ -16,6 +17,26 @@ matchInfo.forEach(function (match) {
         window.location.href = `match.html`;
     })
 })
+
+function remove() {
+    navlinks.forEach(function (navlink) {
+        navlink.classList.remove("selected");
+        var nav = navlink.getAttribute("id");
+        document.getElementById(`${nav}-page`).style.display = "none";
+    })
+}
+remove();
+
+
+navlinks.forEach(function (navlink) {
+    navlink.addEventListener("click", function () {
+        var id = navlink.getAttribute("id");
+        remove();
+        navlink.classList.add("selected");
+        document.getElementById(id+"-page").style.display = "flex";
+    })
+})
+
 
 back.addEventListener("click", function () {
     window.location.href = `index.html`;
